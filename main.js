8d0185981727e1220f7ad88f77b1f48d8a0d317f
pegozzigabriel <pegozzigabriel@gmail.com>
2025-04-08 01:17:42 +0000

    recebendo as porcetangem

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue(){
     bill = document.querySelector("#bill").valueAsNumber 
@@ -9,5 +10,18 @@ function receiveBillValue(){
 
 function receiveNumberOfPeopleValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercentageValue(value){   
+    if(buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+    }
+
+    tipPercentage = value / 100 
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+
+    buttonSelected.classList.add("button-selected")
+    
 }
 
